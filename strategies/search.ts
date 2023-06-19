5f7aa724438324df009681ab4cae21fe089b0c81
Saif Ali Khan <saffellikhan@gmail.com>
2023-06-20 00:51:40 +0500

    Update search.ts

--- strategies/search.ts
+++ strategies/search.ts
@@ -9,69 +9,69 @@ export const SearchStrategies = {
       query?: string;
     }
   ) => {
-    await page.goto("https://google.com", {
-      timeout: DefaultNavigationTimeout,
-    });
-
-    const SearchBoxSelector =
-      '.gLFyf,input[type="text"][aria-label="Google Search"],textarea[aria-label="Search"]';
-
-    await page.waitForSelector(SearchBoxSelector);
-    await page.hover(SearchBoxSelector);
-    await page.click(SearchBoxSelector);
-
-    console.info("Activity::", "Searchbox has been focused!");
-
-    for (let char of options?.query || url.hostname) {
-      await page.waitForTimeout(200);
-      await page.keyboard.type(char);
-    }
-
-    await page.keyboard.press("Enter", { delay: 500 });
-
-    await page.waitForNavigation({
-      timeout: DefaultNavigationTimeout,
-    });
-
-    const linksList = (
-      await page
-        .evaluate(() => {
-          const linkElements = Array.from(document.querySelectorAll("a"));
-          const links = linkElements
-            .filter((element) => element.href)
-            .map((element) => element.href);
-
-          return links;
-        })
-        .catch(console.error)
-    )?.filter((link) => link.includes(url.hostname));
-
-    console.info("Activity::", "Matched Links:", linksList);
-
-    if (!linksList || !linksList.length) {
-      console.warn(
-        "We didn't found the exptected host on the Google's search list!"
-      );
-
-      await page.goto(url.toString());
-
-      return page;
-    }
-
-    const TargetLink = linksList[0];
-    const TargetLinkSelector = `a[href="${TargetLink}"]`;
-
-    await page.mouse.wheel({ deltaY: 300 });
-    await page.hover(TargetLinkSelector);
-
-    console.info("Activity::", "Navigating to:", TargetLink);
-
-    await Promise.all([
-      page.waitForNavigation({
+    try {
+      await page.goto("https://google.com", {
         timeout: DefaultNavigationTimeout,
-      }),
-      page.click(TargetLinkSelector, { delay: 500 }),
-    ]);
+      });
+
+      const SearchBoxSelector =
+        '.gLFyf,input[type="text"][aria-label="Google Search"],textarea[aria-label="Search"]';
+
+      await page.waitForSelector(SearchBoxSelector);
+      await page.hover(SearchBoxSelector);
+      await page.click(SearchBoxSelector);
+
+      console.info("Activity::", "Searchbox has been focused!");
+
+      for (let char of options?.query || url.hostname) {
+        await page.waitForTimeout(200);
+        await page.keyboard.type(char);
+      }
+
+      await page.keyboard.press("Enter", { delay: 500 });
+
+      await page.waitForNavigation({
+        timeout: DefaultNavigationTimeout,
+      });
+
+      const linksList = (
+        await page
+          .evaluate(() => {
+            const linkElements = Array.from(document.querySelectorAll("a"));
+            const links = linkElements
+              .filter((element) => element.href)
+              .map((element) => element.href);
+
+            return links;
+          })
+          .catch(console.error)
+      )?.filter((link) => link.includes(url.hostname));
+
+      console.info("Activity::", "Matched Links:", linksList);
+
+      if (!linksList || !linksList.length)
+        throw new Error(
+          "We didn't found the exptected host on the Google's search list!"
+        );
+
+      const TargetLink = linksList[0];
+      const TargetLinkSelector = `a[href="${TargetLink}"]`;
+
+      await page.mouse.wheel({ deltaY: 300 });
+      await page.hover(TargetLinkSelector);
+
+      console.info("Activity::", "Navigating to:", TargetLink);
+
+      await Promise.all([
+        page.waitForNavigation({
+          timeout: DefaultNavigationTimeout,
+        }),
+        page.click(TargetLinkSelector, { delay: 500 }),
+      ]);
+    } catch (error) {
+      console.warn(error);
+      await page.goto(url.toString());
+    }
 
     return page;
   },
